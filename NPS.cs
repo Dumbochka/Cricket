44ad6cec74738e048845109f1c481fa5084e00f1
Dumbochka <117302071+Dumbochka@users.noreply.github.com>
2022-11-09 20:28:49 +0300

    Add files via upload

---
+++ NPS.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPS : MonoBehaviour
+{
+    public int health = 5;
+
+    public int level = 1;
+
+    public float speed = 1.2f;
+
+
+    void Start()
+    {
+        health += level;
+        print("Жизни: " + health);
+    }
+
+
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+
+        newPosition.z += speed * Time.deltaTime;
+
+        transform.position = newPosition;
+
+    }
+}
